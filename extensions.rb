2f9d7e754b0024270b3870f18723a9a0938043a7
Herb Miller Jr <herb@hlmjr.com>
2016-11-29 21:24:57 -0500

    Applied SILENT_MODE monkey patch to reply and action_reply as well.

--- extensions.rb
+++ extensions.rb
@@ -11,3 +11,29 @@ class Cinch::User
     end
   end
 end
+
+class Cinch::Message
+  def reply(text)
+    if ENV[SILENT_MODE].downcase == 'true'
+      puts "%s: Reply to %s = %s" % [SILENT_MODE, user.nick, text]
+    else
+      text = text.to_s
+      if @channel && prefix
+        text = text.split("\n").map {|l| "#{user.nick}: #{l}"}.join("\n")
+      end
+      reply_target.send(text)
+    end
+  end
+end
+
+class Cinch::Message
+  def action_reply(text)
+    if ENV[SILENT_MODE].downcase == 'true'
+      puts "%s: Action reply to %s = %s" % [SILENT_MODE, user.nick, text]
+    else
+      text = text.to_s
+      reply_target.action(text)
+    end
+  end
+end
+
